173a8804957cc20174f2c87c769184a91fb092eb
Maks Blazhko <makssimm1@gmail.com>
2023-06-28 15:58:30 +0200

    add TaskType model

--- manager/models.py
+++ manager/models.py
@@ -8,7 +8,7 @@ class Position(models.Model):
     class Meta:
         ordering = ["name"]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -21,3 +21,13 @@ class Worker(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
