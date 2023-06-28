cb084c720bf84e66847b3c25f5960f5179f82390
Maks Blazhko <makssimm1@gmail.com>
2023-06-28 18:24:32 +0200

    add null and blank args to position field in worker model

--- manager/models.py
+++ manager/models.py
@@ -13,7 +13,7 @@ class Position(models.Model):
 
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         verbose_name = "worker"
