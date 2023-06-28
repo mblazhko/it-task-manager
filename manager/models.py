d96422a197424ebe58de618e24e38ee205be05a5
Maks Blazhko <makssimm1@gmail.com>
2023-06-28 15:56:11 +0200

    add Position model

--- manager/models.py
+++ manager/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
 
