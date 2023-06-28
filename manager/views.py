c6a5c5ebf6de4aa0c022fb3eb34a938f3c22ff36
Maks Blazhko <makssimm1@gmail.com>
2023-06-28 18:25:18 +0200

    add IndexView, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView to views.py

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from manager.models import Worker, Task, TaskType, Position
+
+
+class IndexView(LoginRequiredMixin, generic.TemplateView):
+    template_name = "manager/index.html"
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context['num_workers'] = Worker.objects.count()
+        context['num_tasks'] = Task.objects.count()
+        context['num_of_done_tasks'] = Task.objects.filter(is_done=True).count()
+        return context
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    template_name = "manager/task_list.html"
+    paginate_by = 10
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("manager:task_list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("manager:task_list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("manager:task_list")
+
+
+
