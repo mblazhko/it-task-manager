594be28ca254582498917529e038999fd22d9d5c
Maks Blazhko <makssimm1@gmail.com>
2023-06-28 18:57:43 +0200

    add index and tasks(list, detail, create, update, delete) urls

--- manager/urls.py
+++ manager/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
+from manager.views import IndexView, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
+
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tasks/", TaskListView.as_view, name="tasks-list"),
+    path("tasks/<int:pk>", TaskDetailView.as_view, name="tasks-detail"),
+    path("tasks/create", TaskCreateView.as_view, name="tasks-create"),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view, name="tasks-update"),
+    path("tasks/<int:pk>/delete", TaskDeleteView.as_view, name="tasks-delete"),
+
 ]
