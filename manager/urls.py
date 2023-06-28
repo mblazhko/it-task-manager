0dd3be00943cd335bc34328d7e110f30fa14c867
Maks Blazhko <makssimm1@gmail.com>
2023-06-28 19:10:58 +0200

    add task type(list, create, update, delete) urls

--- manager/urls.py
+++ manager/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from manager.views import IndexView, TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, \
     WorkerListView, WorkerDetailView, WorkerCreateView, WorkerUpdateView, WorkerDeleteView, PositionListView, \
-    PositionCreateView, PositionUpdateView, PositionDeleteView
+    PositionCreateView, PositionUpdateView, PositionDeleteView, TaskTypeListView, TaskTypeCreateView, \
+    TaskTypeUpdateView, TaskTypeDeleteView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
@@ -20,4 +21,8 @@ urlpatterns = [
     path("position/create", PositionCreateView.as_view, name="position-create"),
     path("position/<int:pk>/update", PositionUpdateView.as_view, name="position-update"),
     path("position/<int:pk>/delete", PositionDeleteView.as_view, name="position-delete"),
+    path("task-types/", TaskTypeListView.as_view, name="task-type-list"),
+    path("task-type/create", TaskTypeCreateView.as_view, name="task-type-create"),
+    path("task-type/<int:pk>/update", TaskTypeUpdateView.as_view, name="task-type-update"),
+    path("task-type/<int:pk>/delete", TaskTypeDeleteView.as_view, name="task-type-delete"),
 ]
