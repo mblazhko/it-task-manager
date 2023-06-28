3a0dec38972c864a9e6fa05aeb546c9021a4bbee
Maks Blazhko <makssimm1@gmail.com>
2023-06-28 15:50:14 +0200

    add Worker model

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
