f9de9f0eec1f7001d1dc75161d2994277a253dd5
Maks Blazhko <makssimm1@gmail.com>
2023-07-06 11:26:36 +0200

    fix status validation name

--- manager/forms.py
+++ manager/forms.py
@@ -50,7 +50,7 @@ class ProjectCreationForm(forms.ModelForm):
     def clean_status(self):
         status = self.cleaned_data.get("status")
 
-        if self.instance.pk is None and status == "completed":
+        if self.instance.pk is None and status == "complete":
             raise ValidationError("Status can't be set as 'completed' during project creation.")
 
         return status
