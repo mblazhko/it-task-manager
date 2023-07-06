71d12d3dc8faabb43dba7c8b9dfaefa10b121578
Maks Blazhko <makssimm1@gmail.com>
2023-07-06 10:42:53 +0200

    fix project get_queryset function

--- manager/views.py
+++ manager/views.py
@@ -9,7 +9,7 @@ from manager.forms import (
     TaskSearchForm,
     WorkerSearchForm,
     PositionSearchForm,
-    TaskTypeSearchForm, ProjectCreationForm, TeamCreationForm,
+    TaskTypeSearchForm, ProjectCreationForm, TeamCreationForm, ProjectSearchForm,
 )
 from manager.models import Worker, Task, TaskType, Position, Project, Team
 
@@ -239,6 +239,26 @@ class ProjectListView(LoginRequiredMixin, generic.ListView):
     model = Project
     paginate_by = 5
     queryset = Project.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProjectListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = ProjectSearchForm(initial={"name": name})
+
+        return context
+
+    def get_queryset(self):
+        form = ProjectSearchForm(self.request.GET)
+
+        if form.is_valid():
+            name = form.cleaned_data.get("name")
+            if name is not None:
+                queryset = Project.objects.filter(name__icontains=name)
+            else:
+                queryset = Project.objects.all()
+            return queryset
 
 
 class ProjectCreateView(LoginRequiredMixin, generic.CreateView):
