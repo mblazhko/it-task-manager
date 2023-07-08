e59b3391ca448b7cd131df540c85a971d7f5320a
Maks Blazhko <makssimm1@gmail.com>
2023-07-08 14:28:52 +0200

    set team list pagination by 4

--- manager/views.py
+++ manager/views.py
@@ -287,7 +287,7 @@ class ProjectDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class TeamListView(LoginRequiredMixin, generic.ListView):
     model = Team
-    paginate_by = 5
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(TeamListView, self).get_context_data(**kwargs)
