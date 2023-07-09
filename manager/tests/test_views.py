70761eb31fb17cf441262ff13f3c8dac66c2bd78
Maks Blazhko <makssimm1@gmail.com>
2023-07-09 10:24:54 +0200

    add private detail and list tests for worker, task type, team and project models

--- manager/tests/test_views.py
+++ manager/tests/test_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from manager.models import Position, Team, TaskType, Project, Task
+from manager.models import Position, Team, TaskType, Project, Task, Worker
 
 POSITION_LIST_URL = reverse("manager:position-list")
 WORKER_LIST_URL = reverse("manager:worker-list")
@@ -56,7 +56,7 @@ class BasePrivateTest(TestCase):
 
 
 class PrivatePositionTest(BasePrivateTest):
-    def test_retrieve_positions(self):
+    def test_retrieve_position_list(self):
         response = self.client.get(POSITION_LIST_URL)
         positions = Position.objects.all()
 
@@ -69,16 +69,77 @@ class PrivatePositionTest(BasePrivateTest):
 
 
 class PrivateWorkerTest(BasePrivateTest):
-    def test_retrieve_positions(self):
+    def test_retrieve_worker_list(self):
         response = self.client.get(WORKER_LIST_URL)
-        positions = Position.objects.all()
+        workers = Worker.objects.all()
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
-            list(response.context["position_list"]),
-            list(positions)
+            list(response.context["worker_list"]),
+            list(workers)
         )
-        self.assertTemplateUsed(response, "manager/position_list.html")
+        self.assertTemplateUsed(response, "manager/worker_list.html")
+
+    def test_retrieve_worker_detail(self):
+        response = self.client.get(WORKER_DETAIL_URL)
+        worker = get_user_model().objects.get(id=self.admin_user.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.context["worker"],
+            worker
+        )
+        self.assertTemplateUsed(response, "manager/worker_detail.html")
+
+
+class PrivateTaskTypeTest(BasePrivateTest):
+    def test_retrieve_task_type_list(self):
+        response = self.client.get(TASK_TYPE_LIST_URL)
+        task_types = TaskType.objects.all()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["task_type_list"]),
+            list(task_types)
+        )
+        self.assertTemplateUsed(response, "manager/task_type_list.html")
+
+
+class PrivateTeamTest(BasePrivateTest):
+    def test_retrieve_team_list(self):
+        response = self.client.get(TEAM_LIST_URL)
+        teams = Team.objects.all()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["team_list"]),
+            list(teams)
+        )
+        self.assertTemplateUsed(response, "manager/team_list.html")
+
+
+class PrivateProjectTest(BasePrivateTest):
+    def test_retrieve_project_list(self):
+        response = self.client.get(PROJECT_LIST_URL)
+        projects = Project.objects.all()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["project_list"]),
+            list(projects)
+        )
+        self.assertTemplateUsed(response, "manager/project_list.html")
+
+    def test_retrieve_project_detail(self):
+        response = self.client.get(PROJECT_DETAIL_URL)
+        project = Project.objects.get(id=self.project.id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.context["project"],
+            project
+        )
+        self.assertTemplateUsed(response, "manager/project_detail.html")
 
 
 class PublicPositionTest(TestCase):
