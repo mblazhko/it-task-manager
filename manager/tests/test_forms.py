c5151df2a9f5ebdf4e95594a6442020a54f0cc14
Maks Blazhko <makssimm1@gmail.com>
2023-07-09 11:03:53 +0200

    add base form test with setUp method

--- manager/tests/test_forms.py
+++ manager/tests/test_forms.py
@@ -3,11 +3,12 @@ from django.test import TestCase
 from django.urls import reverse
 
 from manager.forms import TaskForm, TaskSearchForm
-from manager.models import Team, Project, TaskType
+from manager.models import Team, Project, TaskType, Task, Position
 
 
-class TaskFormTest(TestCase):
+class BaseFormTest(TestCase):
     def setUp(self):
+        super().setUp()
         self.admin_user = get_user_model().objects.create_superuser(
             username="TestAdmin",
             password="testpass12345",
@@ -29,6 +30,22 @@ class TaskFormTest(TestCase):
             name="TestTaskType"
         )
 
+        self.task = Task.objects.create(
+            name="test_task",
+            deadline="2031-01-01",
+            is_completed=False,
+            priority="medium",
+            task_type=self.task_type,
+            project=self.project
+        )
+        self.task.assignees.set([self.admin_user])
+
+        self.position = Position.objects.create(
+            name="test_position",
+        )
+
+
+class TaskFormTest(BaseFormTest):
     def test_task_creation_form_with_non_valid_completed_status(self):
         form_data = {
             "name": "TestTask",
@@ -47,3 +64,5 @@ class TaskFormTest(TestCase):
         form_data = {"keyword": "test"}
         form = TaskSearchForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+
