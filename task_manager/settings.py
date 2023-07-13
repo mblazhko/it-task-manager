bd12a25a7792824d88494df8f516773f9b25f2cc
Maks Blazhko <makssimm1@gmail.com>
2023-07-13 14:12:31 +0200

    remove staticfile_storage settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -161,8 +161,6 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
